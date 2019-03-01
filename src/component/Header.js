61109882cc0d3f7317ebfabfd99d8b75358b83d6
vinod <vinod@olivetech.net>
2019-03-01 18:34:20 +0530

    vsf

---
+++ src/component/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+ class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
